7b68c12218e59a0bc6fe9ed067e261ce96e0ef8a
mkaychuks <sachufusi@gmail.com>
2021-07-28 03:12:09 -0700

    :cloud:

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
+const urlRouter = require('./routes/urlRoutes');
 const app = express(); // init express app
+
+app.use(express.json()); // allow json inputs and res
+app.use('/api/', urlRouter);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Server listening on port 5000...');
