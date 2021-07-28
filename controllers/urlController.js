a106ba22a3a1ea5ce5e97acec06f90fef6c32d9f
mkaychuks <sachufusi@gmail.com>
2021-07-28 04:02:05 -0700

    modified sturvs :cloud:

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,8 +1,10 @@
-const urlHomePage = (request, response) => {
-  response.json({
+const urlHomePage = (req, res) => {
+  res.json({
     message: 'Hello world',
     success: true,
   });
 };
+
+const shortUrl = (req, res) => {};
 
 module.exports = urlHomePage;
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const urlRouter = require('./routes/urlRoutes');
+require('dotenv/config');
 const app = express(); // init express app
 
 app.use(express.json()); // allow json inputs and res
 app.use('/api/', urlRouter);
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log('Server listening on port 5000...');
-});
+mongoose
+  .connect(DB_URI, {
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+      console.log('Server listening on port 5000...');
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
