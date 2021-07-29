8fb90a5f02d4ff0f0ea900d8c182db3a0b50dac7
mkaychuks <sachufusi@gmail.com>
2021-07-29 03:03:33 -0700

    mvc pattern

--- routes/urlRoutes.js
+++ routes/urlRoutes.js
@@ -1,8 +1,14 @@
 const { Router } = require('express');
-const urlHomePage = require('../controllers/urlController');
+const {
+  urlHomePage,
+  shortUrl,
+  sendtoShortenedUrl,
+} = require('../controllers/urlController');
 
 const router = Router();
 
 router.get('/', urlHomePage);
+router.post('/create/', shortUrl); // shortened format of url
+router.get('/:shortUrl', sendtoShortenedUrl);
 
 module.exports = router;
